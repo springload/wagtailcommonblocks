b0fac02173543d56479388d3d71a800301f0b25b
Loic Teixeira <loic@springload.co.nz>
2018-03-22 16:41:08 +1100

    Fix classifier

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'Environment :: Web Environment',
         'Framework :: Django',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
